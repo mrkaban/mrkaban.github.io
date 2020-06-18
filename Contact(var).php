65c11e3ac7a9c3fb479b2a497c507c295513f10e
Алексей <s-r-grass@mail.ru>
2020-06-19 00:58:28 +0700

    Delete Contact(var).php

--- Contact(var).php
+++
@@ -1,4 +0,0 @@
-<?php
-$OESiteUploadDirectory = "Upload";
-$OEConfWETextBox = '{"WE2e809eaca3":{"Title":{"Items":{"DEFAULT":"<span style=\\"color:#971600;\\">*</span> Lorem ipdum :","FR":"<span style=\\"color:#971600;\\">*</span> Lorem ipdum :"}}},"WE964ff19f1d":{"Title":{"Items":{"DEFAULT":"<span style=\\"color:#971600;\\">*</span>&nbsp;Dolor :","FR":"<span style=\\"color:#971600;\\">*</span>&nbsp;Dolor :"}}}}';
-$OEConfWETextArea = '{"WE792e1b24ae":{"Title":{"Items":{"DEFAULT":"Message :","FR":"Message :"}}}}';
